068dbbb860c29ad9736d54213fd7401fd6b3e4b0
Niall Slater <niallslater@gmail.com>
2017-05-31 17:29:55 +0100

    added stuff to CYOA

--- js/CYOAController.js
+++ js/CYOAController.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     gameText = $('#game_Text');
     buttonA.bind("click", PressButtonA);
     buttonB.bind("click", PressButtonB);
-    
+    ReadyButtons();
 });
 
 function PressButtonA() {
@@ -21,9 +21,56 @@ function PressButtonB() {
     RefreshButtons();
 }
 
-function RefreshButtons() {
-    buttonA.hide();
-    buttonB.hide();
+/* Make the buttons pop in once the text has scrolled. */
+function ReadyButtons() {
+    buttonA.css('visibility','hidden');
+    buttonB.css('visibility','hidden');
+    
+    buttonA
+        .delay(1400 + 400)
+        .queue(function (next) { 
+            $(this).css('visibility','visible'); 
+            next();
+    });
+        
+    buttonB
+        .delay(1400 + 900)
+        .queue(function (next) { 
+            $(this).css('visibility','visible'); 
+            next();
+    });
+}
+
+function RefreshButtons(chapterID) {
+    buttonA.css('visibility','hidden');
+    buttonB.css('visibility','hidden');
     
     //fadeout old buttons and fadein the new buttons
+    
+    var optionA = "defaultA";
+    var optionB = "defaultB";
+    
+    switch (chapterID) {
+        case "AA": optionA = "Fight";   optionB = "Run";    break;
+        case "AB": optionA = "Scream";  optionB = "Honk";   break;
+        case "BA": optionA = "Cry";     optionB = "Fist";   break;
+        case "BB": optionA = "Sleep";   optionB = "Bonk";   break;
+    }
+    
+    buttonA.text(optionA);
+    buttonB.text(optionB);
+    
+    buttonA
+        .delay(400)
+        .queue(function (next) { 
+            $(this).css('visibility','visible'); 
+            next();
+    });
+        
+    buttonB
+        .delay(900)
+        .queue(function (next) { 
+            $(this).css('visibility','visible'); 
+            next();
+    });
 }
