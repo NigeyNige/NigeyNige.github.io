f3e4e809555cb0c6dcc9e29b609424a97ccb7849
Niall Slater <niallslater@gmail.com>
2017-07-26 12:29:25 +0100

    Imported new files
    for redesign

---
+++ js/MainController.js
@@ -0,0 +1,18 @@
+/*
+
+$(document).ready(function () {
+    
+    $('.tile').mouseover(function() {
+        $(this).css("background-color", "#fff");
+        $(this).css("color", "#333");
+        
+    });
+    
+    $('.tile').mouseout(function() {
+        
+        $(this).css("background-color", "");
+        $(this).css("color", "");
+    });
+});
+
+*/
