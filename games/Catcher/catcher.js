cc057bafc28e50c1bd459c40379c6a0cd020073e
NigeyNige <niallslater@gmail.com>
2017-10-13 16:36:59 +0100

    added touch control

--- games/Catcher/catcher.js
+++ games/Catcher/catcher.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("Canvas_Catcher");
 var ctx = canvas.getContext("2d");
 
+var canvasScale = 2; //magic number
+
 //Rock Image
 var rockReady = false;
 var rockImage = new Image();
@@ -96,15 +98,18 @@ function Boy(x_modifier)
 }
 
 // INITIALISATION
+
 // Spawn asteroids
 for (i = 0; i < num_Asteroids; i++) {
 	asteroids[i] = new Asteroid();
 }
+
 // Spawn boys
 for (i = 0; i < num_Boys; i++) {
 	boys[i] = new Boy(boy_Interval * i + Math.random() * 100 - 50);
 }
 
+// RESET GAME
 var reset = function () {
 	
 	for (i = 0; i < num_Asteroids; i++) {
@@ -122,6 +127,7 @@ var reset = function () {
 	difficulty = 1;
 };
 
+//INPUT HANDLERS
 
 // Handle keyboard controls
 var keysDown = {};
@@ -138,9 +144,82 @@ addEventListener("keyup", function (e) {
 	delete keysDown[e.keyCode];
 }, false);
 
+//Handle touch controls
+
+// Set up mouse events
+var pressing = false;
+var mousePos = { x:0, y:0 };
+var lastPos = mousePos;
+canvas.addEventListener("mousedown", function (e) {
+        pressing = true;
+  lastPos = getMousePos(canvas, e);
+}, false);
+canvas.addEventListener("mouseup", function (e) {
+  pressing = false;
+}, false);
+canvas.addEventListener("mousemove", function (e) {
+  mousePos = getMousePos(canvas, e);
+}, false);
+
+// Get the position of the mouse relative to the canvas
+function getMousePos(canvasDom, mouseEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: mouseEvent.clientX - rect.left,
+    y: mouseEvent.clientY - rect.top
+  };
+}
+
+//mouse listener
+canvas.addEventListener("touchstart", function (e) {
+        mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
 
 
-//Update function
+//UPDATE FUNCTION
 var update = function (modifier) {
 	
 	difficulty += modifier/10;
@@ -161,6 +240,17 @@ var update = function (modifier) {
 	}
 	if (32 in keysDown) { // Player holding space
 		
+	}
+	
+	if (pressing) {
+		if (mousePos.x > ship.x * canvasScale)
+			ship.x += modifier * ship.speed;
+		if (mousePos.x < ship.x * canvasScale)
+			ship.x -= modifier * ship.speed;
+		if (mousePos.y > ship.y * canvasScale)
+			ship.y += modifier * ship.speed;
+		if (mousePos.y < ship.y * canvasScale)
+			ship.y -= modifier * ship.speed;
 	}
 	
 	//KEEP PLAYER WITHIN BOUNDS
