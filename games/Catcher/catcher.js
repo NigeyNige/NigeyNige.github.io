572f16e171e33cc2d122d3437282b1df3add596b
Niall Slater <niallslater@gmail.com>
2017-10-25 16:30:25 +0100

    debug

--- games/Catcher/catcher.js
+++ games/Catcher/catcher.js
@@ -4,20 +4,8 @@ var ctx = canvas.getContext("2d");
 var debugText = "";
 
 // Prevent scrolling when touching the canvas
-document.body.addEventListener("touchstart", function (e) {
-	if (e.target == canvas) {
-		e.preventDefault();
-	}
-}, false);
-document.body.addEventListener("touchend", function (e) {
-	if (e.target == canvas) {
-		e.preventDefault();
-	}
-}, false);
-document.body.addEventListener("touchmove", function (e) {
-	if (e.target == canvas) {
-		e.preventDefault();
-	}
+$(canvas).on("touchmove", function (e) {
+    e.preventDefault();
 }, false);
 
 
