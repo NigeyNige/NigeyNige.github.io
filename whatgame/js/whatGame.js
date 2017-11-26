6646fe457a1e6d5f7dac51211b300c6230b0cf67
Niall Slater <niallslater@gmail.com>
2017-11-26 22:46:03 +0000

    Whoops, mistaken commit.

--- whatgame/js/whatGame.js
+++ whatgame/js/whatGame.js
@@ -2,69 +2,52 @@ var gamesLoaded = false;
 
 var gameResult;
 
-var gameTitle;
-var gameImage;
-var gameDescription;
-var gameLink;
-
-var newTitle;
-var imageSRC;
-var newDescription;
-var newLink;
+//Grab the existing elements
+var gameTitle = document.getElementById("gameTitle");
+var gameImage = document.getElementById("gameImage");
+var gameDescription = document.getElementById("gameDescription");
+var gameLink = document.getElementById("gameLink");
 
 var selectedGame;
 
-function getNewGame() {
-    
-    //Grab the existing elements
-    
-    gameResult = document.getElementById("gameResult");
-    
-    gameTitle = document.getElementById("gameTitle");
-    gameImage = document.getElementById("gameImage");
-    gameDescription = document.getElementById("gameDescription");
-    gameLink = document.getElementById("gameLink");
-    
-    //Get the details of a random game from the internet
-    
-    newTitle = "newTitle";
-    imageSRC = "";
-    newDescription = "newDescription";
-    newLink = "/";
-    
-    getFeed();
-    
-    gameImage.setAttribute("src", "images/loading.gif");
-    gameTitle.innerHTML = "Thinking...";
-    gameDescription.innerHTML = "";
-    gameLink.setAttribute("href", "");
-}
+var listLoaded = false;
+var loadedGames = [];
 
-function getFeed() {
+var rssFeedURL = "https://itch.io/games/newest.xml";
+
+generateList(30);
+
+var selector = 0;
+
+function generateList(size) {
     
-    //itch.io provides RSS feeds where each game is referred to as an 'item'.
-    //We can get different kinds of results by using other URLs provided by Itch.
-    //The code below will only work with Itch - other stores will need custom code.
-    
-    var numberOfItemsToGrab = 10;
-    
-    var rssFeedURL = "https://itch.io/games/newest.xml";
-    
-    var selector = Math.floor(Math.random() * numberOfItemsToGrab);
+    var numberOfItemsToGrab = size;
     
     $.get(rssFeedURL, function (data) {
-        $(data).find("item").eq(selector).each(function () {
-            selectedGame = $(this);
-            parseFeed();
-        });
+        if (loadedGames.length < size) {
+            $(data).find("item").each(function () {
+                loadedGames.push($(this));
+                console.log("Pushed an item.");
+            });
+            listLoaded = true;
+            if (gameTitle.innerHTML == "Hunting for games...") {
+                gameTitle.innerHTML = "Ready!";
+            }
+            loadedGames = shuffle(loadedGames);
+        }
     });
-    
 }
 
-function parseFeed() {
-        
-    console.log("Selected game is " + selectedGame);
-
+function getNewGame() {
+    
+    if (!listLoaded) {
+        gameTitle.innerHTML = "Hunting for games...";
+        return;
+    }
+    
+    //Get the details of a game from the list
+    selectedGame = loadedGames[selector];
+    
     //Use JQuery's built-in XML support to parse the feed
 
     newTitle = selectedGame.find("plainTitle").text();
@@ -82,12 +65,35 @@ function parseFeed() {
     gameImage.setAttribute("src", imageSRC);
     gameDescription.innerHTML = newDescription;
     gameLink.setAttribute("href", newLink);
+    
+    if (selector < loadedGames.length-1)
+        selector++;
+    else
+        selector = 0;
+}
 
-    //Do a fancy animation
+function shuffle(array) {
+    
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-    //itemToAdd.style.height = "1px";
-    //animate in the width as a wipe type thing
-    //setTimeout(function() { itemToAdd.style.height = "50px";}, 1)
+    return array;
+}
 
-    //.focus() on re-roll button for rapid tapping
+function newList(url) {
+    rssFeedURL = url;
+    loadedGames = [];
+    generateList(30);
+    
+    gameTitle.innerHTML = "New list selected.";
+    gameImage.setAttribute("src", "");
+    gameDescription.innerHTML = "";
+    gameLink.setAttribute("href", "");
 }
