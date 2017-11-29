ea3982c93ef56f27886edff8e177ecbca0b35e77
Niall Slater <niallslater@gmail.com>
2017-11-29 12:58:35 +0000

    fields invisible until data is loaded

--- counted/js/counted.js
+++ counted/js/counted.js
@@ -1,10 +1,15 @@
 
 //Grab the existing elements
+var elements = [];
 var victimName = document.getElementById("victimName");
 var victimAge = document.getElementById("victimAge");
 var victimCause = document.getElementById("victimCause");
 var victimDate = document.getElementById("victimDate");
 
+elements.push(victimName);
+elements.push(victimAge);
+elements.push(victimCause);
+elements.push(victimDate);
 var JSONURL = "https://thecountedapi.com/api/counted";
 
 var selector = 0;
@@ -20,6 +25,11 @@ function getData() {
         rawData = data;
         console.log(rawData);
         displayVictim();
+
+        for (i = 0; i < elements.length; i++) {
+            elements[i].style.opacity = 1;
+        }
+
     });
     
 }
