905c3924b4ac175204b8f05a1fe1c5324614a7d7
niall-slater <niallslater@gmail.com>
2018-05-15 19:59:50 +0100

    Updated webmech

--- games/web-mech/js/index.js
+++ games/web-mech/js/index.js
@@ -7,16 +7,52 @@ var color_water = '#003baa';
 var color_destroyed = '#652300';
 var color_mech = '#fff';
 var color_enemy = '#f00';
+var color_building = '#e3e3e3';
+
+//Map is a 10x10 table element
+//Declare keys for items
+var mapSize = 10;
+var mapID_field = "-";
+var mapID_woods = "◭";
+var mapID_mountains = "△";
+var mapID_water = "~";
+var mapID_destroyed = "#";
+var mapID_mech = "M";
+var mapID_enemy = "X";
+var mapID_building = "▤";
+var mapElement = document.getElementById("map");
+
+let versionNumber = 0.01;
+
+document.getElementById('version').innerText = versionNumber;
+
+//Build the map table before anything else
+for (var x = 0; x < mapSize; x++) {
+    let mapTable = document.getElementById("map");
+    
+    let rowNode = document.createElement('TR');
+    
+    for (var y = 0; y < mapSize; y++) {
+        
+        let cellNode = document.createElement('TD');
+        cellNode.innerText = '0';
+        rowNode.appendChild(cellNode);
+        
+    }
+    
+    mapTable.appendChild(rowNode);
+    
+}
 
 //Game variables
 
-var v_tempMax = 65; //Temperature at which reactor takes damage
-var v_tempMin = 3;
+var v_tempMax = 65;     //Temperature at which reactor takes damage
+var v_buildingsLeft = 0;    //Number of buildings left standing
 
 var mech = {
 	
 	//Values to keep track of health, dangers and resources
-	status : {
+	status: {
 		reactor: 100,
 		temp: 35,
 		fuel: 100,
@@ -24,7 +60,7 @@ var mech = {
 		defense: 20,
 		alive: true
 	},
-	
+    
 	position : {
 		x: 2,
 		y: 2
@@ -34,19 +70,23 @@ var mech = {
 		depleteReactor(amount);
 	},
 	
+	hit: function (damage) {
+		
+	},
+	
 	die: function() {
-		map[this.position.x][this.position.y] = tileDestroyed();
+        destroyTile(this.position.x, this.position.y);
 		if (this.position.x > 0)
-			map[this.position.x-1][this.position.y] = tileDestroyed();
+			destroyTile(this.position.x - 1, this.position.y);
 		if (this.position.x < mapSize-1)
-			map[this.position.x+1][this.position.y] = tileDestroyed();
+			destroyTile(this.position.x + 1, this.position.y);
 		if (this.position.y > 0)
-			map[this.position.x][this.position.y-1] = tileDestroyed();
+			destroyTile(this.position.x, this.position.y - 1);
 		if (this.position.y < mapSize-1)
-			map[this.position.x][this.position.y+1] = tileDestroyed();
+			destroyTile(this.position.x, this.position.y + 1);
 		this.status.alive = false;
 		printToConsole("##REACTOR OVERLOAD##<br />##REACTOR OVERLOAD##<br />##REACTOR OVERLOAD##<br />", true, true);
-		setTimeout(consoleDie, 600);
+		setTimeout(consoleDie, 2000);
 	}
 }
 
@@ -65,6 +105,12 @@ var enemy = {
 		y: 5
 	},
 	
+    brain: {
+        moveTarget: undefined,
+        attackTarget: undefined,
+		readyToFire: false
+    },
+	
 	hit: function (damage) {
 		let damageResult = damage - map[this.position.x][this.position.y].defense;
 		if (damageResult <= 0)
@@ -78,32 +124,19 @@ var enemy = {
 	},
 	
 	die: function() {
-		map[this.position.x][this.position.y] = tileDestroyed();
+        destroyTile(this.position.x, this.position.y);
 		if (this.position.x > 0)
-			map[this.position.x-1][this.position.y] = tileDestroyed();
+			destroyTile(this.position.x - 1, this.position.y);
 		if (this.position.x < mapSize-1)
-			map[this.position.x+1][this.position.y] = tileDestroyed();
+			destroyTile(this.position.x + 1, this.position.y);
 		if (this.position.y > 0)
-			map[this.position.x][this.position.y-1] = tileDestroyed();
+			destroyTile(this.position.x, this.position.y - 1);
 		if (this.position.y < mapSize-1)
-			map[this.position.x][this.position.y+1] = tileDestroyed();
+			destroyTile(this.position.x, this.position.y + 1);
 		this.status.alive = false;
 	}
 	
 }
-
-//Map is a 10x10 table element
-//Declare keys for items
-var mapSize = 10;
-var mapID_field = "-";
-var mapID_woods = ";";
-var mapID_mountains = "^";
-var mapID_water = "~";
-var mapID_destroyed = "#";
-var mapID_mech = "M";
-var mapID_enemy = "X";
-var mapElement = document.getElementById("map");
-
 
 /* TILE DATA */
 
@@ -147,6 +180,16 @@ function tileWoods() {
 	return tile;
 }
 
+function tileBuilding() {
+	let tile = {
+		type: mapID_building,
+		temp: 40,
+		movementCost: 8,
+		defense: 16
+	};
+	return tile;
+}
+
 function tileDestroyed() {
 	let tile = {
 		type: mapID_destroyed,
@@ -168,6 +211,7 @@ function buildMap() {
 	let num_woods = 2;
 	let num_mountains = 1;
 	let num_rivers = 1;
+	let num_buildings = 4;
 	
 	for (var y = 0; y < mapSize; y++) {
 		
@@ -198,6 +242,22 @@ function buildMap() {
 			result[x][riverY] = tileRiver();
 		}
 	}
+    
+    //Add buildings
+	
+	for (var i = 0; i < num_buildings; i++) {
+		let buildingX = 4 + Math.floor(Math.random() * mapSize - 4);
+		let buildingY = 4 + Math.floor(Math.random() * mapSize - 4);
+        
+        while (result[buildingX][buildingY].type === mapID_building) {
+            buildingX = 4 + Math.floor(Math.random() * mapSize - 4);
+            buildingY = 4 + Math.floor(Math.random() * mapSize - 4);
+        }
+        
+        result[buildingX][buildingY] = tileBuilding();
+        v_buildingsLeft++;
+	}
+    
 	
 	//Add enemy
 	
@@ -238,6 +298,7 @@ function updateMechStatus() {
 	
 	let currentTile = map[mech.position.x][mech.position.y];
 	
+	//update temperature
 	let targetTemp = currentTile.temp;
 	let amount = Math.floor((targetTemp-mech.status.temp) / 4);
 	if (mech.status.temp != targetTemp) {
@@ -247,8 +308,14 @@ function updateMechStatus() {
 	//Print warning messages
 	if (mech.status.alive) {
 		if (mech.status.temp > v_tempMax) {
-			printToConsole("Reactor overheating", true, true);
+			printToConsole("REACTOR OVERHEATING", true, true);
 		}
+	}
+	
+	//handle collision
+	if (mech.position.x === enemy.position.x && mech.position.y === enemy.position.y) {
+		mech.damage(10);
+		printToConsole("PROXIMITY ALERT: REACTOR STRESS", true, true);
 	}
 }
 
@@ -296,11 +363,21 @@ function updateMap() {
 				case mapID_woods: 		cells[x].style.color = color_woods; break;
 				case mapID_mountains: 	cells[x].style.color = color_mountains; break;
 				case mapID_water:  		cells[x].style.color = color_water; break;
+				case mapID_building:  	cells[x].style.color = color_building; break;
 				case mapID_destroyed:  	cells[x].style.color = color_destroyed; cells[x].style.textShadow = '0px 0px 20px ' + color_destroyed; break;
 			}
 			
 		}
 	}
+}
+
+function updateGameStatus() {
+    
+    if (v_buildingsLeft <= 0) {
+        printToConsole("ALL BUILDINGS DESTROYED", false, false);
+        printToConsole("MISSION FAILED", false, true);
+		setTimeout(consoleDie, 2000);
+    }
 }
 
 function skipTurn() {
@@ -317,6 +394,7 @@ function endTurn() {
 	enemyTurn();
 	updateMechStatus();
 	updateMap();
+    updateGameStatus();
 }
 
 function movePlayer(direction) {
@@ -372,43 +450,106 @@ function enemyTurn() {
 	
 	//choose direction
 	
+	if (!enemy.status.alive) {
+		return;
+	}
+	
 	var direction;
 	
-	var selector = Math.random();
-	
-	if (selector < .25) {
-		direction = 'n';
-	} else if (selector < .5) {
-		direction = 'e';
-	} else if (selector < .75) {
-		direction = 's';
-	} else {
-		direction = 'w';
-	}
-	
-	if (mech.position.x < enemy.position.x && direction === 'e') {
-		direction = 'w';
-	}
-	if (mech.position.y < enemy.position.y && direction === 's') {
-		direction = 'n';
-	}
-	if (mech.position.y > enemy.position.y && direction === 'n') {
-		direction = 's';
-	}
-	if (mech.position.x > enemy.position.x && direction === 'w') {
-		direction = 'e';
-	}
-	
-	//TODO: check to stop enemy moving onto mech space
-	
-	var moving = Math.random();
-	
-	if (moving > 0.2) {
-		moveEnemy(direction);
-	} else {
-		enemyAttack(direction);
-	}
-	
+    let behaviours = {
+        'wander': 0,
+        'idle': 1,
+        'destroy': 2,
+        'hunt': 3   
+    };
+    
+    let currentBehaviour = behaviours.destroy;
+    
+    //Wander behaviour
+    
+    switch (currentBehaviour) {
+        case behaviours.wander: {
+        
+            var selector = Math.random();
+
+            if (selector < .25) {
+                direction = 'n';
+            } else if (selector < .5) {
+                direction = 'e';
+            } else if (selector < .75) {
+                direction = 's';
+            } else {
+                direction = 'w';
+            }
+
+            if (mech.position.x < enemy.position.x && direction === 'e') {
+                direction = 'w';
+            }
+            if (mech.position.y < enemy.position.y && direction === 's') {
+                direction = 'n';
+            }
+            if (mech.position.y > enemy.position.y && direction === 'n') {
+                direction = 's';
+            }
+            if (mech.position.x > enemy.position.x && direction === 'w') {
+                direction = 'e';
+            }
+
+            var moving = Math.random();
+
+            if (moving > 0.1) {
+                moveEnemy(direction);
+            } else {
+                enemyAttack(direction);
+            }
+            break;
+        }
+        case behaviours.idle: {
+            break;
+        }
+        case behaviours.destroy: {
+			console.log(enemy.brain);
+			
+			//If we don't have a target in mind, get one
+            if (enemy.brain.attackTarget === undefined) {
+                enemy.brain.attackTarget = getRandomTileOfType(mapID_building);
+				if (enemy.brain.attackTarget === undefined)
+					return;
+                enemy.brain.moveTarget = findVantagePoint(enemy.position, enemy.brain.attackTarget);
+            } else if (!enemy.brain.readyToFire) {
+			//Move into position and get ready to fire
+				moveEnemyTowardsVantagePoint(enemy.brain.moveTarget);
+                if (enemy.position.x === enemy.brain.moveTarget.x &&
+					enemy.position.y === enemy.brain.moveTarget.y) {
+					enemy.brain.readyToFire = true;
+					printToConsole("Enemy beam charging.", false, true);
+				}
+			} else {
+			//Fire!
+				let fireVector = 'n';
+
+				if (enemy.brain.attackTarget.y < enemy.position.y)
+					fireVector = 'n';
+				if (enemy.brain.attackTarget.y > enemy.position.y)
+					fireVector = 's';
+				if (enemy.brain.attackTarget.x < enemy.position.x)
+					fireVector = 'w';
+				if (enemy.brain.attackTarget.x > enemy.position.x)
+					fireVector = 'e';
+
+				enemyAttack(fireVector);
+				enemy.brain.attackTarget = undefined;
+				enemy.brain.moveTarget = undefined;
+			}
+			break;
+        }
+        case behaviours.hunt: {
+            break;
+        }
+        default: {
+            break;
+        }
+    }
 }
 
 function moveEnemy(direction) {
@@ -420,6 +561,31 @@ function moveEnemy(direction) {
 		case 'w': if (enemy.position.x > 0) {enemy.position.x--;} break;
 	}
 }
+
+function moveEnemyTowardsVantagePoint(cellReference) {
+	
+    //Vantage point should always be on the same X or Y as the enemy
+    //so we don't need to do any pathfinding. Just move towards the
+    //right cell.
+    
+    if (cellReference.x === enemy.position.x) {
+        if (enemy.position.y > cellReference.y) {
+            enemy.position.y--;
+        } else if (enemy.position.y < cellReference.y) {
+            enemy.position.y++;
+        }
+    } else if (cellReference.y === enemy.position.y) {
+        if (enemy.position.x > cellReference.x) {
+            enemy.position.x--;
+        } else if (enemy.position.x < cellReference.x) {
+            enemy.position.x++;
+        }
+    } else {
+        console.log("Problem: enemy vantage point is not on same X or Y as enemy");
+    }
+    
+}
+
 
 function enemyAttack(direction) {
 	
@@ -435,7 +601,7 @@ function enemyAttack(direction) {
 				mech.hit(enemy.status.attack);
 			}
 			for (var i = 1; i < enemy.position.y + 1; i++) {
-				map[enemy.position.x][enemy.position.y - i] = tileDestroyed();
+                destroyTile(enemy.position.x, enemy.position.y - i);
 			}
 			break;
 		case 'e':
@@ -444,7 +610,7 @@ function enemyAttack(direction) {
 				mech.hit(enemy.status.attack);
 			}
 			for (var i = 1; i < mapSize - enemy.position.x; i++) {
-				map[enemy.position.x + i][enemy.position.y] = tileDestroyed();
+                destroyTile(enemy.position.x + i, enemy.position.y);
 			}
 			break;
 		case 's':
@@ -453,7 +619,7 @@ function enemyAttack(direction) {
 				mech.hit(enemy.status.attack);
 			}
 			for (var i = 1; i < mapSize - enemy.position.y; i++) {
-				map[enemy.position.x][enemy.position.y + i] = tileDestroyed();
+                destroyTile(enemy.position.x, enemy.position.y + i);
 			}
 			break;
 		case 'w':
@@ -462,11 +628,13 @@ function enemyAttack(direction) {
 				mech.hit(enemy.status.attack);
 			}
 			for (var i = 1; i < enemy.position.x + 1; i++) {
-				map[enemy.position.x - i][enemy.position.y] = tileDestroyed();
+                destroyTile(enemy.position.x - i, enemy.position.y);
 			}
 			break;
 		default: console.log("Enemy tried to fire in an invalid direction");
 	}
+	
+	enemy.brain.readyToFire = false;
 	
 	updateMap();
 }
@@ -523,7 +691,7 @@ function attack(direction) {
 				enemy.hit(mech.status.attack);
 			}
 			for (var i = 1; i < mech.position.y + 1; i++) {
-				map[mech.position.x][mech.position.y - i] = tileDestroyed();
+                destroyTile(mech.position.x, mech.position.y - i);
 			}
 			break;
 		case 'e':
@@ -533,7 +701,7 @@ function attack(direction) {
 				enemy.hit(mech.status.attack);
 			}
 			for (var i = 1; i < mapSize - mech.position.x; i++) {
-				map[mech.position.x + i][mech.position.y] = tileDestroyed();
+				destroyTile(mech.position.x + i, mech.position.y);
 			}
 			break;
 		case 's':
@@ -543,7 +711,7 @@ function attack(direction) {
 				enemy.hit(mech.status.attack);
 			}
 			for (var i = 1; i < mapSize - mech.position.y; i++) {
-				map[mech.position.x][mech.position.y + i] = tileDestroyed();
+                destroyTile(mech.position.x, mech.position.y + i);
 			}
 			break;
 		case 'w':
@@ -553,13 +721,13 @@ function attack(direction) {
 				enemy.hit(mech.status.attack);
 			}
 			for (var i = 1; i < mech.position.x + 1; i++) {
-				map[mech.position.x - i][mech.position.y] = tileDestroyed();
+                destroyTile(mech.position.x - i, mech.position.y);
 			}
 			break;
 		default: console.log("Tried to fire in an invalid direction");
 	}
 	
-	changeTemp(15);
+	changeTemp(30);
 	
 	endTurn();
 }
@@ -595,11 +763,77 @@ function consoleDie() {
 	gameConsole.className += " consoleDeath";
 	
 	setTimeout(function() {
-		gameConsole.innerHTML = "<h1 span style='color:#0f0; text-align:center;'>GAME OVER</h1>";
+		gameConsole.innerHTML = "<h1 span style='color:#0f0; text-align:center;'>GAME OVER</h1><h3 span style='color:#0f0; text-align:center;'>Refresh page to play again.</h3>";
 		gameConsole.className = "console consoleDead";
-	}, 800);
+	}, 1500);
 }
 
+function destroyTile(x, y) {
+    
+    if (map[x][y].type === mapID_building) {
+        buildingDestroyed();
+    }
+    
+    map[x][y] = tileDestroyed();
+}
+
+function buildingDestroyed() {
+    v_buildingsLeft--;
+	let remainText = " - " + v_buildingsLeft + " REMAINING";
+	if (v_buildingsLeft < 1) {
+		remainText = "";
+	}
+    printToConsole("BUILDING LOST" + remainText, true, false);
+}
+
+function findVantagePoint(currentPos, targetPos) {
+    
+    //Return the closest cell reference that exists on the same X or Y as the target
+    //This is to find a cell from which the mover can fire a beam on the target
+    
+    let result = {x:5, y:5};
+    
+    var difX = Math.abs(targetPos.x - currentPos.x);
+    var difY = Math.abs(targetPos.y - currentPos.y);
+    
+    if (difX < difY) {
+        result.x = targetPos.x;
+        result.y = currentPos.y;
+    } else {
+        result.x = currentPos.x;
+        result.y = targetPos.y;
+    }
+    
+    return result;
+    
+}
+
+function getRandomTileOfType(seekType) {
+    
+    //Return an {x,y} reference for a random tile of the specified type
+    let listOfChoices = [];
+    
+    for (var x = 0; x < mapSize; x++) {
+        for (var y = 0; y < mapSize; y++) {
+            
+            let tileRef = {x:0,y:0};
+            
+            if (map[x][y].type === seekType) {
+                tileRef.x = x;
+                tileRef.y = y;
+                listOfChoices.push(tileRef);
+            }
+        }
+    }
+    
+    let selector = Math.floor(Math.random() * listOfChoices.length);
+	
+	if (listOfChoices.length === 0) {
+		console.log("No tiles found of type " + seekType);
+	}
+    
+    return listOfChoices[selector];
+}
 
 //Start the game
 var map = buildMap();
