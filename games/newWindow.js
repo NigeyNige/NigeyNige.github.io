50be97c5b21dba2a322759a3cdac5e1c199b48f4
niall-slater <niallslater@gmail.com>
2018-06-30 19:22:48 +0100

    Cleaned up games pages and added Pizza Crash

--- games/newWindow.js
+++ games/newWindow.js
@@ -1,4 +1,8 @@
-function onClick() {
-    let myWindow = window.open("bookshop", "", "width=480, height=270");  // Opens a new window
+function onClick(path) {
+	let myWindow;
+	if (path === 'bookshop')
+    	myWindow = window.open(path, "", "width=480, height=270");  // Opens a new window
+	else if (path === 'web-mech')
+		myWindow = window.open(path, "", "width=800, height=600");
 	myWindow.focus();
 }
